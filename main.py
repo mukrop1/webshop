eddfa9058023c726cb9479fa0d2f56adfbd8be44
Sergey Kropachev <kropachev_1998@mail.ru>
2024-10-04 18:56:41 +0500

    make db+model for product. generator lifespan

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from pydantic import EmailStr, BaseModel
+
 import uvicorn
 
+from core.models import Base, db_helper
 from items_views import router as items_router
 from users.views import router as users_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(items_router)
 app.include_router(users_router)
 
 
 @app.get("/")
 def hello_index():
-    return {"message": "Hello asa!"}
+    return {"message": "Salam aleikum bro!"}
 
-
-@app.get("/hello/")
-def hello(name: str = "world"):
-    name = name.strip().title()
-    return {"message": f"Hello {name}!"}
 
 
 if __name__ == "__main__":
