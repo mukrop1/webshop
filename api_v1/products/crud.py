aac27e4e5312a3bb2dc1d74f032f823735560421
Sergey Kropachev <kropachev_1998@mail.ru>
2024-10-11 19:53:14 +0500

    full crud for products ready

--- api_v1/products/crud.py
+++ api_v1/products/crud.py
@@ -3,7 +3,8 @@ from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import Product
 
-from .schemas import ProductCreate
+from .schemas import ProductCreate, ProductUpdate, ProductUpdatePartial
+
 
 async def get_products(session: AsyncSession) -> list[Product]:
     stmt = select(Product).order_by(Product.id)
@@ -11,12 +12,34 @@ async def get_products(session: AsyncSession) -> list[Product]:
     products = result.scalars().all()
     return list(products)
 
+
 async def get_product(session: AsyncSession, product_id: int):
     return await session.get(Product, product_id)
+
 
 async def create_product(session: AsyncSession, product_in: ProductCreate):
     product = Product(**product_in.model_dump())
     session.add(product)
     await session.commit()
-    await session.refresh(product)
+    # await session.refresh(product)
     return product
+
+
+async def update_product(
+    session: AsyncSession,
+    product: Product,
+    product_update: ProductUpdate | ProductUpdatePartial,
+    partial: bool = False,
+):
+    for name, value in product_update.model_dump(exclude_unset=partial).items():
+        setattr(product, name, value)
+    await session.commit()
+    return product
+
+
+async def delete_product(
+    session: AsyncSession,
+    product: Product,
+):
+    await session.delete(product)
+    await session.commit()
